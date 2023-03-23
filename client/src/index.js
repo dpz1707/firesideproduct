7db14fee0392289f470334fbe9e45eecb32db821
dpz1707 <90921421+dpz1707@users.noreply.github.com>
2023-03-23 05:55:28 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+//import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
+
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 if (process.env.NODE_ENV === 'production') disableReactDevTools()
