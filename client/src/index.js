50c916faf856488672a89c79eb867f8451a2a013
dpz1707 <90921421+dpz1707@users.noreply.github.com>
2023-03-23 07:10:28 -0400

    1

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-//import { BrowserRouter } from "react-router-dom";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+//import { HashRouter as Router } from "react-router-dom";
 
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
